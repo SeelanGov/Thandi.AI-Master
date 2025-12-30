17b627d70c444f467a653bafc69bfe9086a463ad
SeelanGov <seelan.ube@gmail.com>
2025-12-30 23:54:47 +0200

    Force redeploy: Fix assessment flow - currentStep=0.5 for registration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-// Deployment trigger: 2025-12-30T21:48:24.860Z
+// Deployment trigger: 2025-12-30T21:53:44.284Z
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true
